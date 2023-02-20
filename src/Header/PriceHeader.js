6b25464cf23a3619377e172eefddc8a79b7abcb4
iryna skrypka <iratro@gmail.com>
2023-02-20 18:40:03 +0200

    homework4

--- src/Header/PriceHeader.js
+++ src/Header/PriceHeader.js
@@ -1,16 +1,36 @@
+import { useEffect, useState } from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import SelectPriceType from './SelectPriceType';
+import { getCurrentPrice } from '../services/apiService';
+import ErrorModal from '../ErrorModal';
 
 function PriceHeader(props) {
+const [currentPrice, setCurrentPrise] = useState(1);
+const [errorMessage, setErrorMessage] = useState(null);
+
+useEffect(() => {
+  getCurrentPrice()
+  .then(({ success, data, messages}) => {
+    if (!success) {
+      throw messages[0];
+  }
+  setCurrentPrise(data[0].price);
+  })
+  .catch((error) => setErrorMessage(error.toString()));
+  }, []);
+
   return (
+    <>
       <Row className='mb-2'>
         <Col>1 of 3</Col>
         <Col>
         <SelectPriceType {...props} />
         </Col>
-        <Col>3 of 3</Col>
+        <Col>{currentPrice}</Col>
       </Row>
+       <ErrorModal errorMessage={errorMessage} handleClose={() => setErrorMessage(null)} />
+       </>
       );
 }
 
